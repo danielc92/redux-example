c107a639e17d33a157f815bec1c204260d76158e
Daniel Corcoran <danielc.92@outlook.com>
2019-06-30 18:24:46 +1000

    Added product reducer

--- src/reducers/product-reducer.js
+++ src/reducers/product-reducer.js
@@ -1,1 +1,12 @@
 // Products reducer
+
+export default function (state=[], action) {
+    switch (action.type) {
+        case 'ADD_PRODUCT':
+            let state_copy = state;
+            state_copy.push(action.payload.product)
+            return state_copy
+        default:
+            return state
+    }
+}
