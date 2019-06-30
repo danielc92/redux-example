c437a7c7f91433e3d5a3094376e3b3e791c93b93
Daniel Corcoran <danielc.92@outlook.com>
2019-06-30 18:44:15 +1000

    Added props into app component

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,21 @@ import { addProduct } from '../actions/product-actions';
 
 class App extends Component {
     render() {
+        console.log(this.props)
         return (
+            <React.Fragment>
             <div>
                 <h1>Redux Example</h1>
             </div>
+            <h3>USER FROM PROPS</h3>
+            <p>{ this.props.user.name }</p>
+            <h3>PRODUCTS FROM PROPS</h3>
+            <ul>
+                {
+                    this.props.products.map(item => <li>{item.name} - ${item.price}</li>)
+                }
+            </ul>
+            </React.Fragment>
         )
     }
 }
