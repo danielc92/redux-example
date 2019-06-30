b407a808e2a3bf5658e878facaab31fe8a081679
Daniel Corcoran <danielc.92@outlook.com>
2019-06-30 18:23:12 +1000

    Made function for user reducer

--- src/reducers/user-reducer.js
+++ src/reducers/user-reducer.js
@@ -1,1 +1,10 @@
 // User reducer
+
+export default function (state='', action) {
+    switch (action.type) {
+        case 'UPDATE_USER':
+            return action.payload.user
+        default:
+            return state
+    }
+}
