da458c398eb0e58b47c646f0bc29ee6f09dd1dec
Daniel Corcoran <danielc.92@outlook.com>
2019-06-30 18:16:42 +1000

    Added App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Redux Example</h1>
+            </div>
+        )
+    }
+}
+
+export default App
