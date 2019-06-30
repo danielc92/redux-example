bb022fbaecb967d47c5bcfcb6b28c0de31efb747
Daniel Corcoran <danielc.92@outlook.com>
2019-06-30 18:21:41 +1000

    Added Provider , createStore and combineReducers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// The redux store
+// Takes in 3 arguments (reducers, initial state, modifiers)
+store = createStore(
+
+)
+
+
+// Provider wraps the entire App, passing in store as props
+ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
